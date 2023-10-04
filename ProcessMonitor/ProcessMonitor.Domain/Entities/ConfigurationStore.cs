f880abc5db1ad9ca9f896367755c6e6038c9329d
Juan Arturo Montes Castaneda <Juan.MontesCastaneda@sunoco.com>
2023-10-04 12:55:54 -0500

    Add entities to domain

---
+++ ProcessMonitor/ProcessMonitor.Domain/Entities/ConfigurationStore.cs
@@ -0,0 +1,29 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace ProcessMonitor.Domain.Entities;
+
+public class ConfigurationStore : ITableEntity
+{
+    public ConfigurationStore(string partitionKey, string rowKey)
+    {
+        PartitionKey = partitionKey;
+        RowKey = rowKey;
+    }
+
+    public string OData { get; set; } = string.Empty;
+
+    public string Recipients { get; set; }
+
+    public int MinimumCount { get; set; }
+
+    public string MonitorTaskName { get; set; }
+
+    public string PartitionKey { get; set; }
+
+    public string RowKey { get; set; }
+
+    public DateTimeOffset? Timestamp { get; set; }
+
+    public ETag ETag { get; set; }
+}
